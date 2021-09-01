2785e8d64fef7b1a5877a0474ed0f75c7eec2d8e
M.RAY <a.mray47@yahoo.fr>
2021-09-01 19:03:38 +0800

    first lesson's challenge

---
+++ main.js
@@ -0,0 +1,8 @@
+const app = Vue.createApp({
+	data() {
+		return {
+			product: 'Socks',
+			description: "At end of every month, we have some dicount about our socks, Buy one get 30% discount, buy two get 35% discount and buy three above get 40% discount!!!"
+		}
+	}
+});
