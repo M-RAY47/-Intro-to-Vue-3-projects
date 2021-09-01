ad704bdabbae45a24612f4347b34ce3ccb50ceb8
M.RAY <a.mray47@yahoo.fr>
2021-09-01 20:36:28 +0800

    add Lesson3 challenge

--- main.js
+++ main.js
@@ -2,7 +2,11 @@ const app = Vue.createApp({
 	data() {
 		return {
 			product: 'Socks',
-			description: "At end of every month, we have some dicount about our socks, Buy one get 30% discount, buy two get 35% discount and buy three above get 40% discount!!!"
+			description: "At end of every month, we have some dicount about our socks, Buy one get 30% discount, buy two get 35% discount and buy three above get 40% discount!!!",
+			image: './assets/images/socks_green.jpg',
+			url: 'https://www.vuemastery.com/courses/intro-to-vue-3/attribute-binding-vue3/',
+			lesson: "lesson 3",
+			link: "Here is the link to "
 		}
 	}
 });
