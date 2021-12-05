24ad826c65a363c26981a7b191ce6fc8bccc4f09
M-RAY47 <a.mray47@yahoo.fr>
2021-12-05 13:20:48 +0800

    add name and review from clients

--- components/ProductReview.js
+++ components/ProductReview.js
@@ -2,8 +2,22 @@ app.component('productReview', {
 	templete:
 	/*html*/ `
 	<form class="product-review">
-	<input/>
+	<p>
+		<label for=""name">Name:</label>
+		<input id="name" v-model="name" type="text"/>
+	</p>
+	<p>
+		<label for="review">Review:</label>
+		<textarea id="review" name="review" v-model="review" type="text"></textarea>
+	</p>
+
 	</form>
 	
-	`
+	`,
+	data() {
+		return {
+			"name": "",
+			review: null
+		}
+	}
 })
