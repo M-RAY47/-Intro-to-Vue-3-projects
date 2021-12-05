41cd7933d1caf6ce7fd99d872cdb392e324c6db4
M-RAY47 <a.mray47@yahoo.fr>
2021-12-05 13:27:55 +0800

    Add submit button to the form

--- components/ProductReview.js
+++ components/ProductReview.js
@@ -2,15 +2,27 @@ app.component('productReview', {
 	templete:
 	/*html*/ `
 	<form class="product-review">
-	<p>
-		<label for=""name">Name:</label>
-		<input id="name" v-model="name" type="text"/>
-	</p>
-	<p>
-		<label for="review">Review:</label>
-		<textarea id="review" name="review" v-model="review" type="text"></textarea>
-	</p>
-
+		<p>
+			<label for=""name">Name:</label>
+			<input id="name" v-model="name" type="text"/>
+		</p>
+		<p>
+			<label for="review">Review:</label>
+			<textarea id="review" name="review" v-model="review" type="text"></textarea>
+		</p>
+		<p>
+			<label for="rating">Rating:</label>
+			<select id="rating" name="rating">
+				<option>5</option>
+				<option>4</option>
+				<option>3</option>
+				<option>2</option>
+				<option>1</option>
+			</select>		
+		</p>
+		<p>
+			<input type="submit" value="submit" id="submit" name="submit">
+		</p>
 	</form>
 	
 	`,
